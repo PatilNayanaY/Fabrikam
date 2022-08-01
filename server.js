5828e9a2b647a788e3c9824cbd3eac08308bf17f
PatilNayanaY <109787919+PatilNayanaY@users.noreply.github.com>
2022-08-01 17:19:11 +0530

    Nayana's fabrikam Application

--- server.js
+++ server.js
@@ -1,27 +1,46 @@
-var express = require('express');
-var app = express();
-var path=require("path");
-var fs=require("fs");
+var express=require('express');
+var app=express();
 
-//server configuration
-app.use(express.static(path.join(__dirname,'public')));
-
-app.get('/', function (req, res) {
-   res.sendFile(path.join(___dirname + '/index.html'));
+app.get("/",(req, res)=>{
+    res.send(
+            "<h1>Fabrikam</h1>"
+            +"<hr/>"
+            +"<h3>Human Resource</h3>"
+            + "<br/>"
+            + "<ol>"
+            +"<li>Mr. Patil</li>"
+            +"<li>Miss. Deshmukh</li>"
+            +"<li>Mr. Chavan</li>"
+            +"<li>Miss. Kulkarni</li>"
+            +"<li>Mr. Deshpande</li>"
+            + "</ol>"
+    );
 });
- 
 
-app.get('/hello', function (req, res) {
-  console.log("CAlling rest api");
-  var person={firstName:'Ravi',lastName:'Tambade',age:47};
-  res.send(person);
+app.get("/Accountings",(req, res)=>{
+    res.send(
+            "<h1>Accounting Information</h1>"
+            +"<hr/>"
+            +"<h3>Fabrikam's Accounting Information</h3>"
+            + "<br/>"
+            + "<ol>"
+            +"<li>Department Head: Mr. Sonwane</li>"
+            + "</ol>"
+    );
 });
 
- 
-var server = app.listen(8081, function () {
+app.get("/Project_Management",(req, res)=>{
+    res.send(
+            "<h1>Project Management  Information</h1>"
+            +"<hr/>"
+            +"<h3>Fabrikam's Project Management Information</h3>"
+            + "<br/>"
+            + "<ol>"
+            +"<li>Department Head: Mr. Kale</li>"
+            + "</ol>"
+    );
+});
 
-  var host = server.address().address
-  var port = server.address().port
 
-  console.log("Example app listening at 9000");
-})
+var server=app.listen(9000);
+console.log("Fabrikam intranet application running on port 9000");
